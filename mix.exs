95668aa4479262b450e96bced361c5efaa950223
Paul Daigle <j.paul.daigle@gmail.com>
2017-11-15 12:55:27 -0500

    Add a new method for partitioning lists

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Morphix.Mixfile do
   def project do
     [
       app: :morphix,
-      version: "0.0.7",
+      version: "0.0.8",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
