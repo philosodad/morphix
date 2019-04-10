25d8caf1ee3d0259f33b055edf9816537159fc9d
trefoils and samoas <vertex-team+paul+shannon@coxautoinc.com>
2019-04-10 21:49:48 +0000

    Add morphiform!
    combine and genericize stringimorph and atomorph into generic function.
    This function is now public, allowing general transformations on keys.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Morphix.Mixfile do
   def project do
     [
       app: :morphix,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
