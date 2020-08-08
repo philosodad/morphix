5f545b25c697300627b27a589de811f318146b70
Paul Daigle <j.paul.daigle@gmail.com>
2020-08-08 15:16:58 -0400

    limit integer range to fix 0 partition problem

--- test/partiphify_test.exs
+++ test/partiphify_test.exs
@@ -2,19 +2,13 @@ defmodule PartiphifyTest do
   use ExUnit.Case
   use PropCheck
 
-  property "always works" do
-    forall type <- term() do
-      boolean(type)
-    end
-  end
-
   # we want a property test that:
   # gets a list and an integer
   # and checks that the function Morphix.partifify!(list l, integer n)
   # returns a list of length n
   # this checks the number of partitions return
   property "number of partitions is correct" do
-    forall {list, p} <- {list(), integer()} do
+    forall {list, p} <- {list(), integer(1,50)} do
       partitioned = Morphix.partiphify!(list, p)
       Enum.count(partitioned) == p
     end
@@ -23,7 +17,4 @@ defmodule PartiphifyTest do
   # other properties: every item in original list is in partitions
   # the sum of the length of the partitions is the length of the original
   # each partition is +-1 of every other partition
-  def boolean(_) do
-    true
-  end
 end
