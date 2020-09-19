1510d3b4009ccdeb7b09a09b4b41a683bb04859c
Paul Daigle <j.paul.daigle@gmail.com>
2020-09-19 11:14:13 -0500

    Bump elixir base version to 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Morphix.Mixfile do
     [
       app: :morphix,
       version: "0.8.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
